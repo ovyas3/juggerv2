e91574e42ec9b1163f1f907741d4dfb9db4841b8
Om Vyas <om.vyas@instavans.com>
2024-06-18 17:53:23 +0530

    map changes

--- src/components/MapView/TripTracker.tsx
+++ src/components/MapView/TripTracker.tsx
@@ -1,6 +1,6 @@
 'use client'
 import './tripTracker.css'
-import { Icon, latLngBounds } from 'leaflet';
+import L from 'leaflet';
 import { Box, Grid, Button, IconButton, useMediaQuery, CardContent, CardMedia, ButtonBase, FormControlLabel, FormGroup, Switch } from "@mui/material";
 import { MapContainer, Marker, Popup, Polyline, LayersControl, TileLayer } from 'react-leaflet';
 import React, { useEffect, useRef, useState } from "react";
@@ -367,7 +367,7 @@ const ActivityTimeLineChart = (props: any) => {
   </React.Fragment>)
 }
 
-const renderMarkers = (tracking_data: any[], customIcon: Icon): JSX.Element[] => {
+const renderMarkers = (tracking_data: any[], customIcon: L.Icon): JSX.Element[] => {
   return tracking_data.map((point, index) => (
     <Marker key={index} position={[point.geo_point.coordinates[1], point.geo_point.coordinates[0]]} icon={customIcon}>
       <Popup>
@@ -379,7 +379,8 @@ const renderMarkers = (tracking_data: any[], customIcon: Icon): JSX.Element[] =>
 
 const TripTracker = (params: any) => {
   const center: [number, number] = [24.2654256,78.9145218];
-  const mapRef = useRef<any>()
+  // const mapRef = useRef<any>()
+  const [mapRef, setMapRef] = useState<L.Map | null>(null);
   const trip_tracker_data = params.trip_tracker_data;
   const [loadMap, setLoadMap] = useState(false);
   const [fnr_data, setFnrData] = useState({});
@@ -396,7 +397,7 @@ const TripTracker = (params: any) => {
   const handleGPSCheck = (e: any) => {
     setShowGPSTracks(e.target.checked);
   }
-  const customIcon = new Icon({
+  const customIcon = new L.Icon({
     iconUrl: haltIcon,
     iconSize: [14, 14], // Size of the icon
     iconAnchor: [7, 14], // Anchor point of the icon, usually half of the size
@@ -424,14 +425,16 @@ const TripTracker = (params: any) => {
     fetchData();
   }, [trip_tracker_data]);
 
-  useEffect(() => {
-    setTrackingLine(tracking_data.map((e: {geo_point: {coordinates: [number, number]}}) => [e.geo_point.coordinates[1],e.geo_point.coordinates[0]]))
-    if (mapRef.current && trackingLine.length > 0) {
-      const bounds = latLngBounds(trackingLine);
-      mapRef.current.fitBounds(bounds);
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loadMap])
+  // useEffect(() => {
+  //   setTrackingLine(tracking_data.map((e: {geo_point: {coordinates: [number, number]}}) => [e.geo_point.coordinates[1],e.geo_point.coordinates[0]]))
+  //   //@ts-ignore
+  //   if (mapRef && mapRef.current && trackingLine.length > 0) {
+  //     const bounds = L.latLngBounds(trackingLine);
+  //     //@ts-ignore
+  //     mapRef.current.fitBounds(bounds);
+  //   }
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [loadMap])
 
   const mobile = !useMediaQuery("(min-width:800px)");
   return (
@@ -442,111 +445,23 @@ const TripTracker = (params: any) => {
             height: mobile ? "90vh" : "100vh",
         }}
       > 
-        {loadMap ?
-          <MapContainer className="map" center={center} zoom={5} style={{ minHeight: '100%', width: '100%', padding: '0px', zIndex: 0, position: "fixed" }} attributionControl={false} ref={mapRef}>
-                <LayersControl >
-                  <LayersControl.BaseLayer checked name="Street View">
-                    <TileLayer
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                  </LayersControl.BaseLayer>
-                  <LayersControl.BaseLayer name="Satellite View">
-                    <TileLayer
-                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                      url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                    />
-                  </LayersControl.BaseLayer>
-                </LayersControl>
-                {/* <Polygon pathOptions={{ color: 'blue' }} positions={pickupgeofence_decoded} /> */}
-                {renderMarkers(tracking_data, customIcon)}
-                {trackingLine.length && <Polyline pathOptions={{ color:'red'}} positions={trackingLine} />}
-          </MapContainer>
-          :
-          <Box
-            sx={{
-              height: mobile ? "90vh" : "100vh",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",    
-              alignItems: "center",
-              background: "#F0F3F9",
-              p: 1,
-              width: '100%',
-              zIndex: 0,
-              position: mobile ? "relative" : "fixed",
-            }}
-            style={{
-              backgroundImage: `url(${mapPlaceHolder})`,
-            }}
-          >
-            <Grid style={{
-              height: mobile ? '26px' : '70px',
-              fontSize: '14px',
-              display: "flex",
-              marginBottom: mobile ? "30px" : "23px",
-              justifyContent: "center",
-              alignContent: "center",
-              alignItems: "center",
-            }} container spacing={1} justifyContent={"center"}>
-              <Grid style={{
-                display: 'flex',
-                marginLeft: '300px',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }} item xs={12}>
-                {/* <CardMedia
-                  component={"img"}
-                  src={mapPathIcon}
-                  style={{
-                    height: mobile ? '32px' : '64px',
-                    width: mobile ? '32px' : '64px',
-                  }}
-                ></CardMedia> */}
-                <Image
-                    src={ mapPathIcon }
-                    alt="Map Path Icon"
-                    width={30}
-                    height={30}
-                  />
-              </Grid>
-              <Grid style={{
-                textAlign: 'center',
-                marginLeft: mobile ? "0px" : "300px",
-                fontSize: mobile ? '1em' : '1.5em',
-                color: '#42454E',
-              }} item xs={8}>
-                Click the button to see the vehicle on the map.
-              </Grid>
-            </Grid>
-            <Button style={{
-              height: mobile ? '26px' : '50px',
-              width: mobile ? '130px': '150px',
-              fontSize: '14px',
-              display: "flex",
-              marginLeft: mobile ? "0px" : "300px",
-              marginBottom: mobile ? "200px" : "0px",
-              justifyContent: "space-between",
-              alignContent: "center",
-              alignItems: "center",
-            }} variant="contained" onClick={() => setLoadMap(true)}>
-              {/* <CardMedia
-              component={"img"}
-              src={mapViewIcon}
-              style={{
-                height: mobile ? '24px' : '30px',
-                width: mobile ? '24px' : '30px',
-              }}
-              ></CardMedia> */}
-              <Image
-                  src={ mapViewIcon }
-                  alt="Map Path Icon"
-                  width={30}
-                  height={30}
-              />
-              Map View
-            </Button>
-          </Box>
-        }
+      <MapContainer className="map" center={center} zoom={5} style={{ minHeight: '105%',width: '101%', padding: '0px', zIndex: '0', position: 'fixed' }} attributionControl={false} ref={setMapRef} >
+        <div className={"layersControl"} style={{marginTop:'60px'}} >
+          <LayersControl>
+          <LayersControl.BaseLayer checked name="Street View">
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite View">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
+          </LayersControl>
+        </div>
+        </MapContainer>
         {loadMap && <FormGroup className='mapButtons'>
           <FormControlLabel
             disabled={!buttonEnabledGPS}
