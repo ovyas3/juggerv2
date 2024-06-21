d77958f8e2f8e8aa7a15a1c8d16a0046d87066e8
Om Vyas <om.vyas@instavans.com>
2024-06-21 12:59:59 +0530

    suicode

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,12 @@
+'use client'
 import React from "react";
 import Tabsection from "../../components/dashboard/tabSection/tabSection";
 import SideDrawer from "@/components/Drawer/Drawer";
 import Header from "@/components/Header/header";
 import {useTranslations} from 'next-intl';
 
-const page = () => {
-   const t = useTranslations('HEADER');
+const DashboardPage = () => {
+  const t = useTranslations('HEADER');
   return (
    <div>
       <Header title={t('dashboard')}/>
@@ -17,6 +18,6 @@ const page = () => {
   );
 };
 
-page.displayName = 'DashboardPage';
+DashboardPage.displayName = 'DashboardPage';
 
-export default page;
+export default DashboardPage;
