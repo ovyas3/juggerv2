4a9fa341438c28bbee4891f1f771fe5f52adb3e1
dinesh.r <dinesh.r@instavans.com>
2024-07-01 13:39:57 +0530

    RR_Document modal changes are done

--- src/components/RR Modal/RRModal.tsx
+++ src/components/RR Modal/RRModal.tsx
@@ -33,27 +33,27 @@ interface PopupProps {
   isOpen: boolean;
   isClose: () => void;
   rrNumbers: string[];
+  isRRDoc: boolean;
 }
 
-const RRModal: React.FC<PopupProps> = ({ isOpen, isClose, rrNumbers }) => {
+const RRModal: React.FC<PopupProps> = ({ isOpen, isClose, rrNumbers, isRRDoc }) => {
   const [activeRR, setActiveRR] = useState<string | null>(null);
   const [rrDetails, setRRDetails] = useState<any>({});
   const [rrTableData, setRRTableData] = useState<any>([]);
-
-  console.log(rrNumbers);
 
   const handleRRDetails = async (rrNumber: string, index: number) => {
     setActiveRR(index.toString());
     try{
       const response = await httpsGet(`get/rr_document?fnr=${rrNumber}`);
-      console.log(response.data);
       const data = response.data;
       const rrDetail = {
-        fnr: data && data.fnr ? data.fnr : 'N/A',
+        rrNo: data && data.rr_no
+        ? data.rr_no
+        : 'N/A',
         rrDate: data && data.created_at ? service.utcToist(data.created_at, 'dd/MM/yyyy') : 'N/A',
         stationFrom: data && data.shipment && data.shipment.pickup_location && data.shipment.pickup_location.code ? data.shipment.pickup_location.code : 'N/A',
         stationTo: data && data.shipment && data.shipment.delivery_location && data.shipment.delivery_location.code ? data.shipment.delivery_location.code : 'N/A',
-        totalWagons: data && data.no_of_wagons ? data.no_of_wagons : 0,
+        totalWagons: data && data.no_of_wagons ? data.no_of_wagons : 'N/A',
         invoicedNo: data && data.invoice_number ?  data.invoice_number : 'N/A',
         invoicedDate: data && data.invoice_date ? service.utcToist(data.invoice_date, 'dd/MM/yyyy') : 'N/A',
         distance: data && data.distance ? data.distance : 'N/A',
@@ -76,10 +76,10 @@ const RRModal: React.FC<PopupProps> = ({ isOpen, isClose, rrNumbers }) => {
           } )
           : [];
   
+      console.log('rrDetail', rrDetail);
+      console.log('tableData', tableData);
       setRRDetails(rrDetail);
       setRRTableData(tableData);
-      console.log(rrDetail);
-      console.log(tableData);
 
     } catch (error) {
       console.log(error);
@@ -93,130 +93,137 @@ const RRModal: React.FC<PopupProps> = ({ isOpen, isClose, rrNumbers }) => {
   }, [isOpen, rrNumbers]);
 
   return (
-    <React.Fragment>
-       <BootstrapDialog
-        onClose={isClose}
-        className='rrmodal-styles'
-        aria-labelledby="customized-dialog-title"
-        open={isOpen}
-      > 
-      <Box sx={{ fontFamily: "'Plus Jakarta Sans', sans-serif" }}>
-        <div className="rrmodal-container">
-          <div aria-label="close" className="rrmodal-close" onClick={isClose}>
-            <Image src={CloseButtonIcon} alt="Close Button" />
-          </div>
-          <div className="rrmodal-header">
-            <h4 className="rrmodal-heading">Railway Receipt</h4>
-          </div>
-          <div className="rrmodal-body">
-            <div className="rrmodal-sidebar">
-              <div className="rrmodal-sidebar-content">
-                <h4 className="rrmodal-sidebar-heading">RR No.</h4>
-                  <div className="rrmodal-sidebar-details-item">
-                    {rrNumbers.map((rrNumber:string, index: number) => {
-                      return (
-                        <p 
-                        key={index}
-                        className={activeRR === index.toString() ? 'rrmodal-sidebar-details-item-value-active' : 'rrmodal-sidebar-details-item-value'}
-                        onClick={() => handleRRDetails(rrNumber, index)}
-                        >
-                          { rrNumber }
-                      </p>
-                      );   
-                    })}
+    <>
+      {isRRDoc ?
+      <React.Fragment>
+         <BootstrapDialog
+          onClose={isClose}
+          className='rrmodal-styles'
+          aria-labelledby="customized-dialog-title"
+          open={isOpen}
+        > 
+        <Box sx={{ fontFamily: "'Plus Jakarta Sans', sans-serif" }}>
+          <div className="rrmodal-container">
+            <div aria-label="close" className="rrmodal-close" onClick={isClose}>
+              <Image src={CloseButtonIcon} alt="Close Button" />
+            </div>
+            <div className="rrmodal-header">
+              <h4 className="rrmodal-heading">Railway Receipt</h4>
+            </div>
+            <div className="rrmodal-body">
+              <div className="rrmodal-sidebar">
+                <div className="rrmodal-sidebar-content">
+                  <h4 className="rrmodal-sidebar-heading">FNR No.</h4>
+                    <div className="rrmodal-sidebar-details-item">
+                      {rrNumbers.map((rrNumber:string, index: number) => {
+                        return (
+                          <p 
+                          key={index}
+                          className={activeRR === index.toString() ? 'rrmodal-sidebar-details-item-value-active' : 'rrmodal-sidebar-details-item-value'}
+                          onClick={() => handleRRDetails(rrNumber, index)}
+                          >
+                            { rrNumber }
+                        </p>
+                        );   
+                      })}
+                    </div>
+                </div>
+              </div>
+              <div className="rrmodal-content">
+                <div className="rrmodal-content-upper">
+                 {rrDetails && <>
+                  <div className="rrmodal-content-upper-body">
+                  <div className="rrmodal-content-upper-item">
+                     <h4 className="rrmodal-content-upper-item-heading">RR No.</h4>
+                     <p className="rrmodal-content-upper-item-value">{ rrDetails.rrNo }</p>
                   </div>
-              </div>
-            </div>
-            <div className="rrmodal-content">
-              <div className="rrmodal-content-upper">
-               {rrDetails && <>
-                <div className="rrmodal-content-upper-body">
-                <div className="rrmodal-content-upper-item">
-                   <h4 className="rrmodal-content-upper-item-heading">FNR</h4>
-                   <p className="rrmodal-content-upper-item-value">{ rrDetails.fnr }</p>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">RR Date</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.rrDate }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Station From</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.stationFrom }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Station To</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.stationTo }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Total Wagons</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.totalWagons }</p>
+                  </div>
+                 </div>
+                <div className="rrmodal-content-lower-body">
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Invoiced No.</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.invoicedNo }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Invoiced Date</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.invoicedDate }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Distance (km)</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.distance }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Total Weight (MT)</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.totalWeight }</p>
+                  </div>
+                  <div className="rrmodal-content-upper-item">
+                    <h4 className="rrmodal-content-upper-item-heading">Total Freight (₹)</h4>
+                    <p className="rrmodal-content-upper-item-value">{ rrDetails.totalFreight }</p>
+                  </div>
                 </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">RR Date</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.rrDate }</p>
+              </>}
                 </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Station From</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.stationFrom }</p>
-                </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Station To</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.stationTo }</p>
-                </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Total Wagons</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.totalWagons }</p>
-                </div>
-               </div>
-              <div className="rrmodal-content-lower-body">
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Invoiced No.</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.invoicedNo }</p>
-                </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Invoiced Date</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.invoicedDate }</p>
-                </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Distance (km)</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.distance }</p>
-                </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Total Weight (T)</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.totalWeight }</p>
-                </div>
-                <div className="rrmodal-content-upper-item">
-                  <h4 className="rrmodal-content-upper-item-heading">Total Freight (₹)</h4>
-                  <p className="rrmodal-content-upper-item-value">{ rrDetails.totalFreight }</p>
-                </div>
-              </div>
-            </>}
-              </div>
-              <div className="rrmodal-content-lower">
-                <TableContainer component={Paper}>
-                  <Table aria-label="simple table" stickyHeader>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell align="left" className="rrmodal-table-headers">S.No</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">Owning Rly</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">Type</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">Wagon No.</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">CC (T)</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">Tare (T)</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">No. Of Articles</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">Gross Weight (T)</TableCell>
-                        <TableCell align="left" className="rrmodal-table-headers">Actual Wt (T)</TableCell>
-                      </TableRow>
-                    </TableHead>
-
-                    <TableBody>
-                      {rrTableData.map((row: any, index: number) => (
-                        <TableRow key={index}>
-                          <TableCell align="left" className="rrmodal-table-body-items">{index + 1}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.owningRly}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.type}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.wagonNo}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.cc}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.tare}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.noOfArticles}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.grossWeight}</TableCell>
-                          <TableCell align="left" className="rrmodal-table-body-items">{row.actualWt}</TableCell>
+                <div className="rrmodal-content-lower">
+                  <TableContainer component={Paper} className="table-head-container">
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell align="left" className="rrmodal-table-headers">S.No</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">Owning Rly</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">Type</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">Wagon No.</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">CC (MT)</TableCell>
+                          <TableCell align="center" className="rrmodal-table-headers">Tare (MT)</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">No. Of Articles</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">Gross Weight (MT)</TableCell>
+                          <TableCell align="left" className="rrmodal-table-headers">Actual Wt (MT)</TableCell>
                         </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+                      </TableHead>
+                      </Table>
+                  </TableContainer>
+
+                      <TableContainer component={Paper} className="table-body-container">
+                        <Table>
+                        <TableBody>
+                        {rrTableData.map((row: any, index: number) => (
+                          <TableRow key={index}>
+                            <TableCell align="left" className="rrmodal-table-body-items">{index + 1}.</TableCell>
+                            <TableCell align="center" className="rrmodal-table-body-items">{row.owningRly}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.type}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.wagonNo}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.cc}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.tare}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.noOfArticles}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.grossWeight}</TableCell>
+                            <TableCell align="left" className="rrmodal-table-body-items">{row.actualWt}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                        </Table>
+                      </TableContainer>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Box>
+        </Box>
       </BootstrapDialog>
-    </React.Fragment>
+    </React.Fragment> : <></>}
+    </>
   );
 };
 
