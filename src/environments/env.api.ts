8de8209dcab4a132604b47f70b065d1ca72654a4
Temp <temp@Instavanss-Laptop.local>
2024-08-22 01:22:50 +0530

    map view

--- src/environments/env.api.ts
+++ src/environments/env.api.ts
@@ -3,4 +3,5 @@ export const environment = {
     PROD_API_URL: 'https://rms-api.instavans.com/api/mysterio/v1/',
     PROD_API_URL_MANHUNTER: 'https://rms-api.instavans.com/api/manhunter/v1/',
     GOOGLE_MAP_API: 'https://www.google.com/',
+    PROD_BASE_URL:'https://rms.instavans.com/'
 }
