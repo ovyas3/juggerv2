474ad3d2831578c4da118e9c396cd02e73cc4cc6
Dinesh R <rdineshrdp@gmail.com>
2024-11-08 14:10:24 +0530

    Custom styles and Dashboard changes are done

--- src/components/UI/CustomMultiSelect/CustomMultiSelect.tsx
+++ src/components/UI/CustomMultiSelect/CustomMultiSelect.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useRef, useEffect } from 'react';
-import './customMultiSelect.css';
+import './CustomMultiSelect.css';
 
 interface MultiSelectProps {
   value: string[];
