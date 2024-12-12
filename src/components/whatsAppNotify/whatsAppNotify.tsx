f7fd886fee39469b6510629dbdcf7aef6c358c0b
Temp <temp@Instavanss-Laptop.local>
2024-12-12 11:18:57 +0530

    inbound chnages

--- src/components/whatsAppNotify/whatsAppNotify.tsx
+++ src/components/whatsAppNotify/whatsAppNotify.tsx
@@ -9,6 +9,7 @@ import "./whatsAppNotify.css";
 import { Tabs, Tab, Box } from '@mui/material'
 import { styled } from '@mui/system'
 import { httpsPost, httpsGet } from "@/utils/Communication";
+import BPCNotify from "./bpcNotify";
 
 // Custom styled components
 const StyledTabs = styled(Tabs)({
@@ -306,6 +307,7 @@ const WhatsAppNotify = () => {
               <StyledTab label="Delay At Plant" />
               <StyledTab label="Delay At Destination" />
               <StyledTab label="Notification" />
+              {/* <StyledTab label="BPC" /> */}
             </StyledTabs>
           </Box>
           <CustomTabPanel value={value} index={0}>
@@ -433,6 +435,9 @@ const WhatsAppNotify = () => {
                 Clear
               </button>
             </div>  
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={4}>
+            <BPCNotify/>
           </CustomTabPanel>
         </Box>
       </div>
