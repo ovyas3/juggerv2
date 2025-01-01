6cd873b55896a523696b2706900aed6130753c7f
Dinesh R <rdineshrdp@gmail.com>
2025-01-01 16:49:10 +0530

    In Plant Wagons updates are done

--- src/app/etaDashboard/inPlantWagons/inPlantWagons.tsx
+++ src/app/etaDashboard/inPlantWagons/inPlantWagons.tsx
@@ -20,9 +20,9 @@ interface WagonData {
     commodity: string
     wagonCount: number
     placement: string
-    remainingFreeTime: string
+    free_time: string
     dcSlab: string
-    timeElapsed: string
+    elapsed_time: string
     dcCharges: number
 }
 
@@ -34,7 +34,7 @@ const InPlantWagons = () => {
     const [endDate, setEndDate] = useState<any>(today);
 
     const [loading, setLoading] = useState(true);
-    const t = useTranslations("Dashboard");
+    const t = useTranslations("DASHBOARD");
     const router = useRouter();
     const [wagonData, setWagonData] = useState<WagonData[]>([]);
     const componentRef = useRef<HTMLDivElement>(null);
@@ -56,11 +56,11 @@ const InPlantWagons = () => {
             updatedDate.setHours(0, 0, 0, 0); // Set to 12:00 AM
 
             const updatedDateString = updatedDate.toISOString();
-    
+
             const epochTime = timeService.millies(updatedDateString);
             const newStartDate: any = timeService.millies(startDate);
             const newEndDate: any = timeService.millies(endDate);
-            
+
             if (type === 'start') {
                 setStartDate(updatedDate);
                 if (newEndDate >= epochTime) {
@@ -89,15 +89,6 @@ const InPlantWagons = () => {
             if (response.statusCode === 200) {
                 const data = response.data;
                 const wagonDataArr = data && data.length > 0 && data.map((item: any) => {
-                    let remainingFreeTime = '';
-                    if (item.timeElapsed) {
-                        const hours = Math.floor(item.elapsed_time / 60);
-                        const minutes = item.elapsed_time % 60;
-                        remainingFreeTime = `${hours.toString().padStart(2, '0')}: ${minutes.toString().padStart(2, '0')}`;
-                    } else {
-                        remainingFreeTime = '';
-                    }
-
                     return {
                         _id: item._id,
                         indent: item.indent || '',
@@ -105,9 +96,9 @@ const InPlantWagons = () => {
                         commodity: item.commodity || '',
                         wagonCount: item.wagonCount || 0,
                         placement: item.placement || '',
-                        remainingFreeTime: remainingFreeTime,
+                        free_time: item.free_time || '',
                         dcSlab: item.slab || '',
-                        timeElapsed: item.timeElapsed || '',
+                        elapsed_time: item.elapsed_time || 0,
                         dcCharges: item.dc_charges || 0
                     }
                 });
@@ -120,6 +111,34 @@ const InPlantWagons = () => {
             setLoading(false);
         }
     };
+
+    const calculateRemainingTime = (placement: string, freeTime: string) => {
+        const targetTime = new Date(new Date(placement).getTime() + parseInt(freeTime) * 3600000);
+        const currentTime = new Date();
+        const diff = targetTime.getTime() - currentTime.getTime();
+
+        if (diff <= 0) return "00:00:00";
+
+        const hours = Math.floor(diff / (1000 * 60 * 60)).toString().padStart(2, "0");
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0");
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000).toString().padStart(2, "0");
+
+        return `${hours}:${minutes}:${seconds}`;
+    };
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setWagonData((prevData) => {
+                return (Array.isArray(prevData) ? prevData : []).map((row) => ({
+                    ...row,
+                    remainingTime: calculateRemainingTime(row.placement, row.free_time),
+                }));
+            });
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
 
     useEffect(() => {
         if (startDate && endDate) {
@@ -178,19 +197,19 @@ const InPlantWagons = () => {
 
     if (loading) {
         return (
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <ThreeCircles
-              visible={true}
-              height="100"
-              width="100"
-              color="#20114d"
-              ariaLabel="three-circles-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-            />
-          </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <ThreeCircles
+                    visible={true}
+                    height="100"
+                    width="100"
+                    color="#20114d"
+                    ariaLabel="three-circles-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
+            </div>
         );
-      }
+    }
 
     return (
         <main className="in-plant-wagons-main" ref={componentRef}>
@@ -316,12 +335,12 @@ const InPlantWagons = () => {
                             <th className="in-plant-wagons-id-column">Remaining Free Time</th>
                             <th className="in-plant-wagons-id-column">DC Slab</th>
                             <th className="in-plant-wagons-id-column">Time elapsed beyond free time</th>
-                            <th className="in-plant-wagons-id-column">DC Charges RS</th>
+                            <th className="in-plant-wagons-id-column">DC Charges (₹)</th>
                         </tr>
                     </thead>
                     <tbody>
                         {wagonData && wagonData.length > 0 ?
-                            wagonData.map((row: any, index: number) => (
+                            wagonData.sort((a, b) => new Date(b.placement).getTime() - new Date(a.placement).getTime()).map((row: any, index: number) => (
                                 <tr key={row._id}>
                                     <td>{index + 1}</td>
                                     <td>
@@ -343,11 +362,11 @@ const InPlantWagons = () => {
                                         <span>{timeService.utcToistTime(row.placement, 'HH:mm') || 'N/A'}</span>
                                     </td>
                                     <td>
-                                        {row.remainingFreeTime}
+                                        {row.remainingTime || 'Calculating...'}
                                     </td>
                                     <td>{row.dcSlab}</td>
-                                    <td>{row.timeElapsed}</td>
-                                    <td className={row.remainingFreeTime === '00:00' ? 'in-plant-wagons-charges' : ''}>{row.dcCharges.toLocaleString()}</td>
+                                    <td>{row.elapsed_time}</td>
+                                    <td className={row.remainingTime === "00:00:00" ? 'in-plant-wagons-charges' : ''}>{row.dcCharges.toLocaleString()}</td>
                                 </tr>
                             )) : (
                                 <tr>
