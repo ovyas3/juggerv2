fe59638663bcd1e8804806e891ec1e9809baa560
Om Vyas <om.vyas@instavans.com>
2025-01-24 21:57:09 +0530

    changes

--- src/utils/storageService.ts
+++ src/utils/storageService.ts
@@ -1,25 +1,71 @@
-'use client'
-import { setCookie, getCookie, hasCookie, deleteCookie } from 'cookies-next';
-
-
-const setCookies = (name: string, value: string) => {
-    setCookie(name, value, {
-        maxAge: 30 * 24 * 60 * 60,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-    })
+"use client";
+interface CookieOptions {
+  domain?: string;
+  expires?: Date | string | number;
+  secure?: boolean;
+  httpOnly?: boolean;
+  path?: string;
 }
 
-const deleteAllCache = () => {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-        const cookieName = cookies[i].split("=")[0].trim();
-        if (cookieName) {
-            deleteCookie(cookieName);
-        }
+function setCookies(name: string, value: string, options: CookieOptions = {}) {
+  let cookieString = `${name}=${value}`;
+  if (options.domain) cookieString += `; domain=${options.domain}`;
+   cookieString += `; path=${options.path || '/'}`;
+  if (options.expires) {
+     const expires = options.expires instanceof Date ? options.expires.toUTCString()
+     : (typeof options.expires === 'number' ? new Date(Date.now() + options.expires * 24 * 60 * 60 * 1000).toUTCString()
+      : options.expires);
+    cookieString += `; expires=${expires}`;
+  }
+  if (options.secure) cookieString += '; secure';
+  if (options.httpOnly) cookieString += '; httpOnly';
+  document.cookie = cookieString;
+}
+
+function getCookie(name: string, path:string = '/') {
+    const nameLenPlus = (name.length + 1);
+    const cookieValue = document.cookie
+        .split(';')
+        .map(c => c.trim())
+        .filter(cookie => {
+            return cookie.substring(0, nameLenPlus) === `${name}=`;
+        })
+        .map(cookie => {
+            return decodeURIComponent(cookie.substring(nameLenPlus));
+        })[0] || null;
+    if(!cookieValue) {
+       const cookieValuePath = document.cookie
+           .split(';')
+           .map(c => c.trim())
+          .filter(cookie => {
+            return cookie.substring(0, nameLenPlus) === `${name}=` && cookie.includes(`path=${path}`);
+          })
+          .map(cookie => {
+             return decodeURIComponent(cookie.substring(nameLenPlus));
+          })[0]
+        return cookieValuePath
     }
-
-    localStorage.clear();
+    return cookieValue;
 }
 
-export { setCookies, getCookie, hasCookie, deleteCookie, deleteAllCache }
+function deleteCookie(name: string, options: CookieOptions = {}) {
+    setCookies(name, '', { ...options, expires: new Date(0) });
+}
+
+
+function deleteAllCache(options: CookieOptions = {}) {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        deleteCookie(name, options);
+    }
+}
+
+function hasCookie(name: string, path: string = '/') {
+  return !!getCookie(name, path);
+}
+
+export { setCookies, getCookie, hasCookie, deleteCookie, deleteAllCache };
