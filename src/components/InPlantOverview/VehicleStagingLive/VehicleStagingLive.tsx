e145bfa0deae35985460b1d4735999705ba27c41
Dinesh R <rdineshrdp@gmail.com>
2025-03-21 17:46:15 +0530

    Vehicle Staging Live is Done

--- src/components/InPlantOverview/VehicleStagingLive/VehicleStagingLive.tsx
+++ src/components/InPlantOverview/VehicleStagingLive/VehicleStagingLive.tsx
@@ -10,6 +10,9 @@ import { useRouter } from "next/navigation";
 import { useSnackbar } from "@/hooks/snackBar";
 import { ThreeCircles } from "react-loader-spinner";
 import VehicleStagingLiveCard from "./VehicleStagingLiveCard/VehicleStagingLiveCard";
+import VehicleStagingLiveShipmentDetails from "./VehicleStagingLiveShipmentDetails/VehicleStagingLiveShipmentDetails";
+import { Download } from 'lucide-react';
+import * as XLSX from 'xlsx';
 
 // Define types for the data structure
 interface Stage {
@@ -22,9 +25,22 @@ interface MaterialEntry {
     };
 }
 
-const stages: MaterialKey[] = ['TW', 'GO', 'TC', 'PO', 'CNCL', 'GI', 'EW', 'EP', 'DC', 'PG'];
+const stages: MaterialKey[] = ['DC', 'PO', 'GI', 'TW', 'GW', 'PG', 'TC', 'IV', 'EW', 'GO'];
 
-type MaterialKey = 'TW' | 'GO' | 'TC' | 'PO' | 'CNCL' | 'GI' | 'EW' | 'EP' | 'DC' | 'PG';
+type MaterialKey = 'DC' | 'PO' | 'GI' | 'TW' | 'GW' | 'PG' | 'TC' | 'IV' | 'EW' | 'GO';
+
+const stageWithName: Record<MaterialKey, string> = {
+    DC: 'DO - PO',
+    PO: 'PO - GI',
+    GI: 'GI - TW',
+    TW: 'TW - GW',
+    GW: 'GW - PG',
+    PG: 'PG - TC',
+    TC: 'TC - IV',
+    IV: 'IV - EW',
+    EW: 'EW - GO',
+    GO: 'GI - GO'
+}
 
 export default function VehicleStagingLive() {
     // Date States
@@ -45,6 +61,9 @@ export default function VehicleStagingLive() {
     const [vehicleStagingLiveData, setVehicleStagingLiveData] = useState<MaterialEntry[]>([]);
     const [sortColumn, setSortColumn] = useState<MaterialKey>("TW");
     const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc")
+    const [driverIds, setDriverIds] = useState<string[]>([]);
+    const [title, setTitle] = useState<string>('');
+    const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false)
 
     const handleStartDateChange = (date: Date | null) => {
         setStartDate(date);
@@ -90,93 +109,144 @@ export default function VehicleStagingLive() {
         return sortDirection === "asc" ? aCount - bCount : bCount - aCount;
     });
 
-    const calculateTotal = (stage: string) => {
-        return vehicleStagingLiveData && vehicleStagingLiveData?.length > 0 && Object.values(vehicleStagingLiveData[0]).reduce((sum, material) => sum + (material[stage]?.count || 0), 0)
+    const handleDriverIDs = (data: any, stage: MaterialKey, material: string) => {
+        const driverIds = data && data?.driverData && data?.driverData?.length > 0 ? data?.driverData : [];
+        if (driverIds && driverIds?.length > 0) {
+            setDriverIds(driverIds);
+            setTitle(`${material} - ${stage}`);
+            setIsPopupOpen(true);
+        } else {
+            showMessage(`No driver data available for ${material} - ${stage}`, "error");
+        }
+    }
+
+    const exportToExcel = () => {
+        const workbook = XLSX.utils.book_new();
+        
+        const headers = ['Materials', ...Object.values(stageWithName)];
+
+        const data = 
+            sortedMaterials && 
+            sortedMaterials.length > 0 && 
+            sortedMaterials?.map(([material, data]) => {
+                return [
+                    material,
+                    ...stages.map(stage => data[stage]?.count || 0)
+                ];
+            }) || [];
+
+        const wsData = [headers, ...data];
+        const worksheet = XLSX.utils.aoa_to_sheet(wsData);
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Vehicle Staging Live');
+        XLSX.writeFile(workbook, 'vehicle_staging_live.xlsx');
     };
 
     return (
         <>
             {/* Loader */}
-            {loading && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', position: 'absolute', width: '100vw', background: 'white', zIndex: 100, opacity: 0.5 }}>
-                <ThreeCircles
-                    visible={true}
-                    height="100"
-                    width="100"
-                    color="#20114d"
-                    ariaLabel="three-circles-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                />
-            </div>}
-            <div
-                className={styles.container}
-                style={{ margin: !mobile ? '56px 0 0 70px' : '0px' }}>
-                {mobile && (
-                    <h1 className={styles.title}>Vehicle Staging Live</h1>
-                )}
-                <Box sx={{
+            {loading && (
+                <div style={{
                     display: 'flex',
-                    justifyContent: 'flex-start',
+                    justifyContent: 'center',
                     alignItems: 'center',
-                    gap: 2,
-                    width: !mobile ? '20%' : '100%',
+                    height: '100vh',
+                    position: 'absolute',
+                    width: '100vw',
+                    background: 'white',
+                    zIndex: 1000,
+                    opacity: 1
                 }}>
-                    <CustomDatePicker
-                        label="From"
-                        value={startDate}
-                        onChange={handleStartDateChange}
-                        maxDate={endDate}
-                        defaultDate={oneWeekAgo}
-                        maxSelectableDate={today}
+                    <ThreeCircles
+                        visible={true}
+                        height="100"
+                        width="100"
+                        color="#20114d"
+                        ariaLabel="three-circles-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
                     />
-                    <CustomDatePicker
-                        label="To"
-                        value={endDate}
-                        onChange={handleEndDateChange}
-                        minDate={startDate}
-                        defaultDate={today}
-                        minSelectableDate={startDate}
-                    />
-                </Box>
-                {mobile && (
-                    <div className={styles.cardContainer}>
-                        {sortedMaterials && sortedMaterials.length > 0 && sortedMaterials.map(([material, data]) => (
-                            <VehicleStagingLiveCard key={material} material={material} data={data} />
-                        ))}
+                </div>
+            )}
+            <div className={styles.main} style={{ margin: !mobile ? '56px 0 0 70px' : '0px' }}>
+                <div className={styles.container}>
+                    {mobile && (
+                        <h1 className={styles.title}>Vehicle Staging Live</h1>
+                    )}
+                    <div className={styles.dateContainer}>
+                        <Box sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-start',
+                            alignItems: 'center',
+                            gap: 2,
+                            width: !mobile ? '20%' : '100%',
+                        }}>
+                            <CustomDatePicker
+                                label="From"
+                                value={startDate}
+                                onChange={handleStartDateChange}
+                                maxDate={endDate}
+                                defaultDate={oneWeekAgo}
+                                maxSelectableDate={today}
+                            />
+                            <CustomDatePicker
+                                label="To"
+                                value={endDate}
+                                onChange={handleEndDateChange}
+                                minDate={startDate}
+                                defaultDate={today}
+                                minSelectableDate={startDate}
+                            />
+                        </Box>
+                        <button className={styles.exportButton} onClick={exportToExcel}>
+                            <Download className={styles.exportButtonIcon} />
+                            Export
+                        </button>
                     </div>
-                )}
-                {!mobile && (
-                    <div className={styles.tableContainer}>
-                        <table className={styles.table}>
-                            <thead>
-                                <tr>
-                                    <th onClick={() => handleSort("TW")}>Materials</th>
-                                    {stages.map((stage) => (
-                                        <th key={stage} onClick={() => handleSort(stage)}>
-                                            {stage}
-                                        </th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {sortedMaterials && sortedMaterials.length > 0 && sortedMaterials.map(([material, data], index) => (
-                                    <tr key={material} className={index === sortedMaterials.length - 1 ? styles.totalRow : ''}>
-                                        <td>{material}</td>
-                                        {stages.map((stage) => (
-                                            <td key={stage}>{data[stage]?.count || 0}</td>
-                                        ))}
+                    {mobile && (
+                        <div className={styles.cardContainer}>
+                            {sortedMaterials && sortedMaterials.length > 0 && sortedMaterials.map(([material, data]) => (
+                                <VehicleStagingLiveCard key={material} material={material} data={data} />
+                            ))}
+                        </div>
+                    )}
+                    {!mobile && (
+                        <div className={styles.tableContainer}>
+                            <table className={styles.table}>
+                                <thead>
+                                    <tr>
+                                        <th onClick={() => handleSort("TW")}>Materials</th>
+                                        {stages.map((stage) => {
+                                            const stageName = stageWithName[stage]
+                                            return (
+                                                <th key={stage} onClick={() => handleSort(stage)}>
+                                                    {stageName}
+                                                </th>
+                                            )
+                                        })}
                                     </tr>
-                                ))}
-                                {/* <tr className={styles.totalRow}>
-                                <td>TOTAL</td>
-                                {stages.map((stage) => (
-                                    <td key={stage}>{calculateTotal(stage)}</td>
-                                ))}
-                            </tr> */}
-                            </tbody>
-                        </table>
-                    </div>
-                )}
+                                </thead>
+                                <tbody>
+                                    {sortedMaterials && sortedMaterials.length > 0 && sortedMaterials.map(([material, data], index) => {
+                                        return (
+                                            <tr key={material} className={index === sortedMaterials.length - 1 ? styles.totalRow : ''}>
+                                                <td>{material}</td>
+                                                {stages.map((stage) => (
+                                                    <td key={stage} onClick={() => handleDriverIDs(data[stage], stage, material)}>{data[stage]?.count || 0}</td>
+                                                ))}
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
+                    <VehicleStagingLiveShipmentDetails
+                        isOpen={isPopupOpen}
+                        onClose={() => setIsPopupOpen(false)}
+                        title={title}
+                        data={driverIds}
+                    />
+                </div>
             </div>
         </>
     )
