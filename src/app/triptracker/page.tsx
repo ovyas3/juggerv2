5b41102553ee3cfba0964ab4aacab1781eb66801
agarsha.selvaraj <agarsha.selvaraj@instavans.com>
2025-09-10 11:22:25 +0530

    new trip tracker

---
+++ src/app/triptracker/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import Triptracker from "@/components/triptracker/triptracker"
+import { useMediaQuery, useTheme } from "@mui/material"
+import { useSearchParams } from "next/navigation";
+export default function TriptrackerPage() {
+  const theme = useTheme()
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"))
+  const sp = useSearchParams();
+
+  // const uniqueCode ="TbaTlSmC50";
+  // const uniqueCode ="xl4hU5SGXz";
+  // const uniqueCode ="xl4hU5SGXz";
+  const uniqueCode ="6BQiTLRQ1N";
+  
+  // const uniqueCode ="nE3ZXGJQKZ";
+    // sp.get("unique_code") ?? sp.get("code") ?? "";
+  return (
+    <div>
+      <Triptracker uniqueCode={uniqueCode}></Triptracker>
+    </div>
+  )
+}
