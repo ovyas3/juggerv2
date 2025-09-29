93a481b05497fdc5405407ef82875e09c6448847
akilaramesh1910 <akilaramesh1910@gmail.com>
2025-09-29 16:05:16 +0530

    a

--- src/environments/env.api.ts
+++ src/environments/env.api.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    API_URL: 'https://live-api.instavans.com/api/thor/v1/',
-    API_URL_DASHBOARD: 'https://live-api.instavans.com/api/thanos/v1/',
-    API_URL_DASHBOARD2: 'https://live-api.instavans.com/api/thanos/v2/',
-    API_URL_DASHBOARD3: 'https://live-api.instavans.com/api/thanos2/v1/',
+    API_URL: 'https://dev-api.instavans.com/api/thor/v1/',
+    API_URL_DASHBOARD: 'https://dev-api.instavans.com/api/thanos/v1/',
+    API_URL_DASHBOARD2: 'https://dev-api.instavans.com/api/thanos/v2/',
+    API_URL_DASHBOARD3: 'https://dev-api.instavans.com/api/thanos2/v1/',
     GOOGLE_MAP_API: 'https://www.google.com/',
     BASE_URL:'https://smartdashbaord.instavans.com/',
     DEV_ETMS:'https://etms.instavans.com/',
@@ -10,9 +10,9 @@ export const environment = {
     TRACKER_URL: 'https://tracker.instavans.com/track/',
     TRIP_TRACKER: 'https://etms-tracker.instavans.com/track/',
     TRACKER_URL_PREFIX: "https://etms-tracker.instavans.com/track/",
-    API_URL_NEW_1: 'https://live-api.instavans.com/api/thor/',
-    API_URL_NEW_2: 'https://live-api.instavans.com/api/thor/v2/',
-    API_URL_NEW_3: 'https://live-api.instavans.com/api/panther/',
+    API_URL_NEW_1: 'https://dev-api.instavans.com/api/thor/',
+    API_URL_NEW_2: 'https://dev-api.instavans.com/api/thor/v2/',
+    API_URL_NEW_3: 'https://dev-api.instavans.com/api/panther/',
     API_URL_NEW_4: 'https://dev-api.instavans.com/api/flash/',
     NEXT_PUBLIC_GOOGLE_MAP_API_KEY:'AIzaSyDr0k02Q0b6SF2xum80HvY7I4kAWUCOR2U'
 }
